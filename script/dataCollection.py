dfdaf1eff034eb645d4d351892a0553c3cb2759b
Nenonymo <nchentre666@gmail.com>
2021-02-28 04:39:56 +0100

    first update, bring the server initialization

--- script/dataCollection.py
+++ script/dataCollection.py
@@ -2,22 +2,63 @@
 
 import mariadb
 import sys
+import os
 
 
-try:
-    conn = mariadb.connect(
-        user="root",
-        password="TBzk0922k3*",
-        host="localhost",
-        port=3306,
-        database="moneybot"
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
-
-cur = conn.cursor()
-
-print(cur)
+#get the connection parameters
+this_file = os.path.dirname(os.path.abspath(__file__))
+dbConfigFile = os.path.join(this_file, '..'+os.sep+'notes.txt')
 
 
+def initializeDBConnection(dbConnectionConfigPath, startLine): #Works perfectly
+    with open(dbConnectionConfigPath, 'r') as f:
+        dbArgsUntreated = f.readlines()[startLine:startLine+5]
+        dbArgs = dict()
+        for i in dbArgsUntreated:
+            a = i.replace(' ', '').strip('\n').split(sep='=')
+            dbArgs[a[0]] = a[1]
+
+    try:
+        conn = mariadb.connect(
+            user=dbArgs['User'],
+            password=dbArgs['Password'],
+            host=dbArgs['Host'],
+            port=int(dbArgs['Port']),
+            database=dbArgs['Database']
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
+    cursor = conn.cursor()
+
+    return((cursor, conn))
+
+def createIfNotExisting (serverID, serverName, cursor, connection): #Works but don't save the name of the server
+    try:
+        cursor.execute("SELECT COUNT(id) AS nbr FROM server WHERE id=?", (serverID,))
+        for (nbr) in cursor:
+            if nbr[0] != 0:
+                return(1)
+        cursor.execute("INSERT INTO server (id,name) VALUES (?, ?)", (serverID,''))
+        connection.commit()
+        return(0)
+    except  mariadb.Error as e:
+        print(e)
+        return(2)
+
+def moneyTransfer (cursor, conn, servID, id1, id2, amount): #Don't work
+    try:
+        cursor.execute("SELECT userid, serverid, moneyAmount FROM userserver WHERE userid=? AND serverid=?", (id1, servID))
+        for (uID, sID, mA) in cursor:
+            print(uID, sID, mA)
+            if mA < amount:
+                return 1
+        
+        cursor.execute("UPDATE userserver SET moneyAmount = moneyAmount - ? WHERE userid=? AND serverid=?", (amount, id1, servID))
+        cursor.execute("UPDATE userserver SET moneyAmount = moneyAmount + ? WHERE userid=? AND serverid=?", (amount, id2, servID))
+        conn.commit()
+        return(0)
+
+    except mariadb.Error:
+        print("Error with the command: one of the users didn't initialized his account")
+        return(2)
--- script/main.py
+++ script/main.py
@@ -2,7 +2,31 @@
 
 
 import discord
+import os
+from datetime import datetime
 
+import dataCollection
+
+######################
+# Configuration File #
+######################
+this_file = os.path.dirname(os.path.abspath(__file__))
+os.chdir(this_file+'..'+os.sep+'..'+os.sep+'.')
+
+#################
+# DB Connection #
+#################
+cursor, connection = dataCollection.initializeDBConnection("notes.txt", 6)
+
+
+
+with open("notes.txt", 'r') as f:
+    botToken = f.readlines()[3].strip('\n')
+
+
+##################
+# Discord Client #
+##################
 
 class MyClient(discord.Client):
 
@@ -11,13 +35,37 @@ class MyClient(discord.Client):
         print(self.user.name)
         print(self.user.id)
         print('------')
+        
 
     async def on_message(self, message):
         #Security for no auto-reply
         if message.author.id == self.user.id:
             return
-        print(message.guild.id, message.channel.id, message.author.id)
+        
+        elif message.content.startswith('$'):
+            #log the command
+            with open('botCommands.log', 'a') as f: #open the file in 'append' mode
+                f.write('{} : {} : {} : {}\n'.format( #write the following string
+                    datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), #datetime of the message
+                    message.guild.id, message.author.id, #serverID and author ID
+                    message.content)) #content of the message
+
+            if message.content.startswith('$initialize server'):
+                print(message.guild.name)
+                sig = dataCollection.createIfNotExisting(message.guild.id, message.guild.name, cursor, connection)
+                if sig == 2: await message.channel.send("The server initialization didn't worked.") #command error
+                elif sig == 1: await message.channel.send("The server had already been initialized.") #server already in DB
+                elif sig == 0: await message.channel.send("Server initialized succesfully") #server added to the db
+            '''
+            if message.content.startswith('$money transfer'):
+                args = message.content[16:].split(sep=' ')
+                args[0] = args[0][3:21]
+            '''
+
+
+
+
+
 
 discordBot = MyClient()
-discordBot.run("ODE1Mjg4NzY0MjQ2MzkyODgz.YDqO1A.fDSJdlFMOmdPOlJ9s9ZAtqSHwkM")
-    
+discordBot.run(botToken)
